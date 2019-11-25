2e616cc15adc1313ae191a787e4020f67febfc63
Semenov Denis <orbancool@mail.ru>
2019-11-26 00:33:44 +0300

    hw01

---
+++ 1l_SD/1l_SD/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  1l_SD
+//
+//  Created by Irina Semenova on 25/11/2019.
+//  Copyright © 2019 Denis Semenov. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController: UITextFieldDelegate{
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+class RoundButton: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height / 2 * 0.5
+    }
+}
+
+class ViewController: UIViewController {
+    @IBOutlet var loginField: UITextField!
+    @IBOutlet var passField: UITextField!
+    var vibrateMode = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginField.delegate = self
+        passField.delegate = self
+        
+        let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(hideAction)
+    }
+
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func vibrateEnter() {
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
+        generator.impactOccurred()
+    }
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        vibrateEnter()
+        guard let log = loginField.text,
+              let pass = passField.text else {
+            return
+        }
+        print("login: \(log)\npassword: \(pass)")
+
+    }
+}
+
